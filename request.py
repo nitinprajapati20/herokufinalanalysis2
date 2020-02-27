396eec4e2a59957469ac5774e147e1cc3828daa3
nitinprajapati20 <61464540+nitinprajapati20@users.noreply.github.com>
2020-02-27 22:35:07 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,11 +2,11 @@
 """
 Created on Sun Feb 23 23:39:16 2020
 
-@author: admin
+@author: admin"""
 
 import requests
 
 url= 'http://localhost:5000/predict_api'
 r= requests.post(url,json={'Enrolled':2, 'Present':7, 'Absent':5, 'genders':0, 'chemistryscore':5, 'mathscore':2})
 
-print(r.json())"""
+print(r.json())
